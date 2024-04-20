b8134f5f64047b0c6e877b48bf913708e39d7310
Tobby Lie <tobbylie@gmail.com>
2024-04-20 15:53:31 +0000

    Update color nvim theme

--- nvim/lua/tlie/plugins.lua
+++ nvim/lua/tlie/plugins.lua
@@ -1,6 +1,6 @@
 return {
     {'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = {'nvim-lua/plenary.nvim'}},
-    {'ellisonleao/gruvbox.nvim'},
+    {'rebelot/kanagawa.nvim'},
     {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
     {'nvim-treesitter/playground'},
     {'theprimeagen/harpoon'},
