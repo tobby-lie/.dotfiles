3b074b11a9f285a150ee246782ffbd864cec9510
Tobby Lie <tobbylie@gmail.com>
2024-05-09 16:56:18 +0000

    Update tf nvim-comment config

--- nvim/after/plugin/nvim-comment.lua
+++ nvim/after/plugin/nvim-comment.lua
@@ -1,3 +1,10 @@
 -- Setup nvim-comment
 require('nvim_comment').setup()
 
+-- Set commentstring for Terraform files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "terraform",
+  callback = function()
+    vim.bo.commentstring = "# %s"
+  end,
+})
