ad02d6647a2528155e4f3c0017d3d98417ff4498
Tobby Lie <tobbylie@gmail.com>
2025-03-25 08:14:08 -0600

    fix lsp file

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,15 +1,12 @@
 local lsp_zero = require('lsp-zero')
-
-
 lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
-
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
@@ -18,14 +15,12 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  -- Replace the language servers listed here
-  -- with the ones you want to install
-  ensure_installed = {'dockerls', 'pyright', 'sqlls', 'terraformls', 'lua_ls', 'docker_compose_language_service', 'bashls', },
+  ensure_installed = {'ts_ls', 'dockerls', 'pyright', 'sqlls', 'terraformls', 'lua_ls', 'docker_compose_language_service', 'bashls', 'gopls', 'clangd'},
   handlers = {
     lsp_zero.default_setup,
   }
 })
-require 'lspconfig'.pyright.setup {}
+
 lsp_zero.set_sign_icons({
   error = '✘',
   warn = '▲',
